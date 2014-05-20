96df4d845dc7378a36d1f70c2340a449fb9870d2
xSirus <vanh97@hotmail.com>
2014-05-20 11:56:35 -0600

    But I understand the need for it

--- Project3/helloWorld.cpp
+++ Project3/helloWorld.cpp
@@ -3,7 +3,7 @@
 // File:       helloWorld.cpp
 // Instructor: Craig Gardner
 // Class:      CS 2450
-// Assignment: Project 3
+// Assignment: 4
 // Date:       5/20/2014
 // 
 // The purpose of this program is to get familiar with source control (git).
@@ -14,8 +14,8 @@ using namespace std;
 
 int main()
 {
-	cout << "\nHello Craig!\n\n";
-	// A better way to do it?
+	cout << "\nThere is no spoon\n\n";
+	// A better way to do it?  Maybe.  Who's to say?
 	cout << "Press the \"ANY\" key to continue...";
 	cin.get();
 	return 0;
