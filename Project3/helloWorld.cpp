502453bbb859a57ade1065e40d2c97ca4e96de58
xSirus <vanh97@hotmail.com>
2014-05-20 11:08:25 -0600

    i am committing

---
+++ Project3/helloWorld.cpp
@@ -0,0 +1,20 @@
+// File Prologue
+// Name:       Joel Harrison
+// File:       helloWorld.cpp
+// Instructor: Craig Gardner
+// Class:      CS 2450
+// Assignment: Project 3
+// Date:       5/20/2014
+// 
+// The purpose of this program is to get familiar with source control (git).
+// -----------------------------------------------------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "\nHello Craig!\n\n";
+	system("PAUSE");// Apparently bad code.
+	return 0;
+}
